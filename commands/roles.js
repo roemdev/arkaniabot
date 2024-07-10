93eaa9bc9079c58201ee476f74811447ce52c0a4
emivirdisla <emivirdisla@gmail.com>
2024-07-10 13:23:40 +0000

    .

--- commands/roles.js
+++ commands/roles.js
@@ -12,7 +12,7 @@ module.exports = {
             .setDescription('Aquí tienes la lista de los roles en Arkania.')
             .addFields(
                 { name: 'Roles principales', value:
-                '* <@&991405352660779028> Fundador del gremio.\n* <@&1251292331852697623> Sub-fundador del gremio.\n* <@&991490018151514123> Segundos al mendo.\n* <@&1215767915329228890> Miembros del gremio.'},
+                '* <@&991405352660779028> Fundador del gremio.\n* <@&1251292331852697623> Sub-fundador del gremio.\n* <@&991490018151514123> Segundos al mendo.'},
                 { name: 'Roles por nivel', value: '* <@&1247699315908935680> Nivel 50.\n* <@&1251306364878458931> Nivel 40.\n* <@&1234893710588645426> Nivel 30.\n* <@&1247707042492452944> Nivel 20.\n* <@&1247613884458729575> Nivel 10.'}
             );
 

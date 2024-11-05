dbe14f90c569ad83804294fa04a97802303522d2
emivirdisla <emivirdisla@gmail.com>
2024-11-05 19:24:21 +0000

    .

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("node:fs");
 const path = require("node:path");
-const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const { Client, Collection, GatewayIntentBits, REST, Routes } = require("discord.js");
 require("dotenv").config();
 
 const client = new Client({
@@ -18,7 +18,8 @@ client.commands = new Collection();
 const commandsPath = path.join(__dirname, "commands");
 const commandFilesAndFolders = fs.readdirSync(commandsPath);
 
-// Carga de comandos
+const commands = [];
+// Carga de comandos y preparación de comandos para deploy
 for (const item of commandFilesAndFolders) {
   const itemPath = path.join(commandsPath, item);
   if (fs.statSync(itemPath).isDirectory()) {
@@ -30,6 +31,7 @@ for (const item of commandFilesAndFolders) {
       const command = require(filePath);
       if ("data" in command && "execute" in command) {
         client.commands.set(command.data.name, command);
+        commands.push(command.data.toJSON());
       } else {
         console.log(
           `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
@@ -40,6 +42,7 @@ for (const item of commandFilesAndFolders) {
     const command = require(itemPath);
     if ("data" in command && "execute" in command) {
       client.commands.set(command.data.name, command);
+      commands.push(command.data.toJSON());
     } else {
       console.log(
         `[WARNING] The command at ${itemPath} is missing a required "data" or "execute" property.`
@@ -64,4 +67,27 @@ for (const file of eventFiles) {
   }
 }
 
-client.login(process.env.BOT_TOKEN);
+// Función para registrar los comandos y luego iniciar el bot
+(async () => {
+  const token = process.env.DISCORD_TOKEN;
+  const clientId = process.env.CLIENT_ID;
+  const guildId = process.env.GUILD_ID;
+  const rest = new REST({ version: "10" }).setToken(token);
+
+  try {
+    console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+    // Registrar comandos en el servidor
+    const data = await rest.put(
+      Routes.applicationGuildCommands(clientId, guildId),
+      { body: commands }
+    );
+
+    console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+
+    // Iniciar sesión en Discord después de registrar los comandos
+    client.login(token);
+  } catch (error) {
+    console.error(error);
+  }
+})();
